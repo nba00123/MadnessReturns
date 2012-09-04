188110cd58d5ffc25bbb79dcb9d837eeb4be9f1e
nba00123 <nba001234@gmail.com>
2012-09-04 21:12:40 +0800

    test

--- MadnessReturns/src/urls.py
+++ MadnessReturns/src/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
+#ff
 urlpatterns = patterns('',
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
      url(r'^admin/', include(admin.site.urls)),
